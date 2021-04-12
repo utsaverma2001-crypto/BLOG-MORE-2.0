37f01aacf2830b586830b1671a4f2cb1967dbf1d
Utsav Verma <utsaverma2001@gmail.com>
2021-04-12 09:56:28 +0530

    initial commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,55 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+
+import "../styling/home.css";
+
+const Homepage = () => {
+  const isSignedIn = useSelector(selectSignedIn);
+
+  const dispatch = useDispatch();
+  const login = (response) => {
+    console.log(response);
+    dispatch(setSignedIn(true));
+    dispatch(setUserData(response.profileObj));
+  };
+
+  return (
+    <div className="home__page" style={{ display: isSignedIn ? "none" : "" }}>
+      {!isSignedIn ? (
+        <div className="login__message">
+          <h1>BLOG MORE</h1>
+          <p>
+            A website for high quality online resource for reading blogs. Just sign
+            up and start reading some quality blogs.
+          </p>
+          <GoogleLogin
+            clientId="696365228527-7vv89pkqnksfp5ejqfivmb2rnpe31qoc.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="login__button"
+              >
+                Login with Google
+              </button>
+            )}
+            onSuccess={login}
+            onFailure={login}
+            isSignedIn={true}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Homepage;
